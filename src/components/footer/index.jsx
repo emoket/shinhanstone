da4e36f8fb6b3efba747754f513fc8c5a1be44ec
박용진 <insight.emoket@gmail.com>
2020-11-29 14:53:21 +0900

    footer 수정

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,8 +4,8 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    신한석재 | 대표: 곽근영 | 126-09-66082 | 경기도 광주시 초월읍 장담길 88-4<br />
-    (TEL) 031-765-4723 | (FAX) 031-765-4724 | mailto:shstonekr@naver.com<br /><br />
+    신한석재 . 곽근영 . 126-09-66082 . 경기도 광주시 초월읍 장담길 88-4<br />
+    📞 <a href="tel:031-765-4723">031-765-4723</a> | 📠 031-765-4724 | <a href="mailto:shstonekr@naver.com?subject=문의">shstonekr@naver.com</a> <br /><br />
     ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
